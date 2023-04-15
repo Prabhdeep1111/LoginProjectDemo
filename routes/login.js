e855f26370ce0baeb3a33840c9b36f66c904388a
Prabhdeep1111 <129708055+Prabhdeep1111@users.noreply.github.com>
2023-04-15 14:04:03 +1200

    Update login.js
    File reviewed

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database');
 
+/* add comments for this function */
 router.get("/", (req, res) => {
     res.render("login");
 })
